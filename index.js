47624f1365802fd89abb9f9800ac672c6cd64363
Andrew Crites <acrites@mobiquityinc.com>
2016-07-29 14:01:02 -0400

    Including CORS support

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const http = require("http");
 
 const PORT = process.env.PORT || 3003;
 const server = http.createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Request-Method", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    if ("OPTIONS" === req.method) {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
     const num = +req.url.split("/")[1];
 
     if (isNaN(num)) {
